6d466532915fe92e792875d287b0afad85cb815e
LENOVO <wahyureva638@gmail.com>
2026-05-06 11:04:43 +0700

    garap an ku va

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:kas_app/pages/auth/splash_screen.dart';
+
+void main() {
+  runApp(Kas());
+}
+
+class Kas extends StatefulWidget {
+  const Kas({super.key});
+
+  @override
+  State<Kas> createState() => _KasState();
+}
+
+class _KasState extends State<Kas> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
+  }
+}
---
+++ lib/pages/auth/splash_screen.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  late Timer t;
+  @override
+  void initState() {
+    t = Timer.periodic(Duration(milliseconds: 3000), (timer) {
+      if (!mounted) return;
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        height: size.height,
+        width: size.width,
+        decoration: BoxDecoration(color: Colors.white),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(height: 150),
+            Icon(
+              Icons.account_balance_wallet_outlined,
+              size: 95,
+              color: Colors.teal,
+            ),
+            Text(
+              "APK KAS",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 300),
+          ],
+        ),
+      ),
+    );
+  }
+}
